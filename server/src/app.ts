625a3ff3eed143e6f73a13e315d780d88b20846f
Lempereur Corentin <73390812+aredli16@users.noreply.github.com>
2025-10-13 22:43:55 +0200

    Add Tailwind CSS integration and update app structure with question handling

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,12 @@
 import express from 'express'
 import questions from "./routes/questions";
+import cors from "cors"
 
 const app = express()
+
+app.use(cors({
+    origin: "http://localhost:5173"
+}))
 
 app.use(express.json())
 
