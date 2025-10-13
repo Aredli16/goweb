907f9b57f74c70a793eb1328000d1545047e27c2
Lempereur Corentin <73390812+aredli16@users.noreply.github.com>
2025-10-13 17:46:34 +0200

    Implement backend

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express'
+import questions from "./routes/questions";
 
 const app = express()
 
 app.use(express.json())
 
 // Routes
+app.use('/api/questions', questions)
 
 export default app
---
+++ server/src/services/questionService.ts
@@ -0,0 +1,33 @@
+import path from "node:path";
+import * as fs from "node:fs";
+
+interface Question {
+    id: number;
+    question: string;
+    options: {id: string, label: string}[];
+    next?: Record<string, number>
+}
+
+function loadQuestions(): Question[] {
+    const filePath = path.join(__dirname, "../data/questions.json");
+    const data = fs.readFileSync(filePath, "utf-8");
+    return JSON.parse(data);
+}
+
+export function getFirstQuestion() {
+    const questions = loadQuestions();
+    return questions[0];
+}
+
+export function getNextQuestion(currentQuestionId: number, answerId: string) {
+    const questions = loadQuestions();
+    const current = questions.find(q => q.id === currentQuestionId);
+
+    if (!current)  return null;
+
+    const nextId = current.next?.[answerId];
+    if (!nextId) return null;
+
+    const nextQuestion = questions.find(q => q.id === nextId);
+    return nextQuestion || null;
+}
